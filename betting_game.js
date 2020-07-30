3e5a6bfa8ac8968b03b7bd5ecaf443d22f235aeb
Lillie <lillieuph@gmail.com>
2020-07-30 17:03:11 -0400

    Add pikachu image, center buttons and add padding, add a separate array for human readable card values and modify existing card values to fix logic errors

--- betting_game.js
+++ betting_game.js
@@ -1,4 +1,5 @@
-var cardValue = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const humanValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 var winner;
 
 function getRandomIntInclusive() {
@@ -10,35 +11,48 @@ function getRandomIntInclusive() {
 }
 
 function highBetCheck() {
+    var edit_img = document.getElementById("windisplay");
+
     if (pikachuDraw < playerDraw) {
+        edit_img.style.backgroundImage = 'url("img/pikahappy.gif")';
         var winner = "You win!";
     } else if (pikachuDraw > playerDraw) {
+        edit_img.style.backgroundImage = 'url("img/sadpika.gif")';
         var winner = "You lose!";
     } else if (pikachuDraw = playerDraw) {
+        edit_img.style.backgroundImage = 'url("img/drawpika.png")';
         var winner = "Draw!";
     }
 
-    console.log(winner);
+    winDisplay.innerHTML = winner;
 }
 
 function lowBetCheck() {
+    var edit_img = document.getElementById("windisplay");
+
     if (pikachuDraw > playerDraw) {
+        edit_img.style.backgroundImage = 'url("img/pikahappy.gif")';
         var winner = "You win!";
     } else if (pikachuDraw < playerDraw) {
+        edit_img.style.backgroundImage = 'url("img/sadpika.gif")';
         var winner = "You lose!";
     } else if (pikachuDraw = playerDraw) {
+        edit_img.style.backgroundImage = 'url("img/drawpika.png")';
         var winner = "Draw!";
     }
 
-    console.log(winner);
+    winDisplay.innerHTML = winner;
 }
 
 function generateButtons() {
     var highBet = document.createElement("button");
     highBet.innerHTML = "↑";
+    highBet.setAttribute("class", "center");
+    highBet.setAttribute("id", "buttons");
 
     highBet.addEventListener("click", function() {
-        playerInfo.innerHTML = "Your draw: " + playerDraw;
+        playerInfo.innerHTML = "Your draw: " + playerDrawHmn;
+        playerInfo.setAttribute("id", "playinfo");
         highBetCheck();
     });
     
@@ -47,40 +61,47 @@ function generateButtons() {
 
     var lowBet = document.createElement("button");
     lowBet.innerHTML = "↓";
+    lowBet.setAttribute("class", "center");
+    lowBet.setAttribute("id", "buttons");
 
     lowBet.addEventListener("click", function() {
-        playerInfo.innerHTML = "Your draw: " + playerDraw;
+        playerInfo.innerHTML = "Your draw: " + playerDrawHmn;
+        playerInfo.setAttribute("id", "playinfo");
         lowBetCheck();
     });
 
     document.body.appendChild(lowBet);
 
-/*     var reset = document.createElement("button");
+    var reset = document.createElement("button");
     reset.innerHTML = "Reset";
+    reset.setAttribute("class", "center");
+    reset.setAttribute("id", "buttons");
 
     reset.addEventListener("click", function() {
-        alert("Pikachu draws a new card");
-        var pikachuInfo = document.createElement("div");
-        var pikachuSeed = getRandomIntInclusive();
-        var pikachuDraw = cardValue[pikachuSeed];
-        pikachuInfo.innerHTML = "Pikachu's draw: " + initialDraw(pikachuDraw);
+        window.location.reload();
     });
 
-    document.body.appendChild(reset); */
+    document.body.appendChild(reset);
 }
+
+var winDisplay = document.createElement("div");
+winDisplay.setAttribute("id", "windisplay");
+winDisplay.setAttribute("class", "pikaheader");
+document.body.appendChild(winDisplay);
 
 
 var pikachuInfo = document.createElement("div");
+pikachuInfo.setAttribute("id", "pikainfo");
 var pikachuSeed = getRandomIntInclusive();
 var pikachuDraw = cardValue[pikachuSeed];
-pikachuInfo.innerHTML = "Pikachu's draw: " + pikachuDraw;
-
+var pikachuDrawHmn = humanValue[pikachuSeed];
+pikachuInfo.innerHTML = "Pikachu's draw: " + pikachuDrawHmn;
 document.body.appendChild(pikachuInfo);
 
 var playerInfo = document.createElement("div");
 var playerSeed = getRandomIntInclusive();
 var playerDraw = cardValue[playerSeed];
-
+var playerDrawHmn = humanValue[playerSeed];
 document.body.appendChild(playerInfo);
 
 generateButtons();
